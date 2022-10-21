60bdf8bf4ecfb876149063ef071389395c2df9d0
Dean Lofts <dean@deanlofts.xyz>
2022-10-21 14:12:52 +1100

    trigger gh actions

--- lambda_function.rb
+++ lambda_function.rb
@@ -2,5 +2,5 @@ require 'json'
 
 def lambda_handler(event:, context:)
     # TODO implement
-    { statusCode: 200, body: JSON.generate('This is a Ruby script running on AWS Lambda!') }
+    { statusCode: 200, body: JSON.generate('This is a Ruby script running on AWS Lambda! Modified via GitHub Actions') }
 end
