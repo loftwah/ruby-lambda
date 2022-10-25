f32c91efa7b3fa8367908e7ec860938b1d09cad9
Dean Lofts <dean@deanlofts.xyz>
2022-10-25 12:41:55 +1100

    remove httparty

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,14 +1,14 @@
-require 'httparty'
+# require 'httparty'
 require 'json'
 
 def lambda_handler(event:, context:)
 
-    response = HTTParty.get('https://api.github.com/users/loftwah')
+    # response = HTTParty.get('https://api.github.com/users/loftwah')
 
     { statusCode: 200,
         body: {
             message: "This is just an AWS Lambda function, not using SAM",
-            response: response.body
+            # response: response.body
           }.to_json
         }
 
