e07b4cf54c4f26fd9db762c2393fda4ada5fbf36
Dean Lofts <dean@deanlofts.xyz>
2022-10-25 12:40:33 +1100

    test new function

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,6 +1,15 @@
+require 'httparty'
 require 'json'
 
 def lambda_handler(event:, context:)
-    # TODO implement
-    { statusCode: 200, body: JSON.generate('This is a Ruby script running on AWS Lambda! Modified via GitHub Actions') }
+
+    response = HTTParty.get('https://api.github.com/users/loftwah')
+
+    { statusCode: 200,
+        body: {
+            message: "This is just an AWS Lambda function, not using SAM",
+            response: response.body
+          }.to_json
+        }
+
 end
